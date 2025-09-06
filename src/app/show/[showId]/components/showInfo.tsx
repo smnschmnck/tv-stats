c159c59272ebeae6f02fe03f5f1b4f1cdffb528a
Simon Schmeinck <simon.schmeinck@gmx.de>
2025-09-07 01:57:12 +0200

    show poster in show details

--- src/app/show/[showId]/components/showInfo.tsx
+++ src/app/show/[showId]/components/showInfo.tsx
@@ -6,9 +6,27 @@ import { ExternalIds } from "@/types/tmdbApi/tvShow";
 import { Suspense } from "react";
 import { Season } from "./season";
 import { cacheLife } from "next/dist/server/use-cache/cache-life";
+import { TVShowDetails } from "@/types/tmdbApi/tvShowDetails";
 
 export const ShowInfo = async ({ showId }: { showId: string }) => {
   cacheLife("hours");
+
+  const detailsRes = await fetch(
+    `https://api.themoviedb.org/3/tv/${showId}?language=en-US`,
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${env.TMDB_SECRET_ACCESS_KEY}`,
+      },
+    }
+  );
+
+  if (!detailsRes.ok) {
+    return <p>Show not found</p>;
+  }
+
+  const showDetails = (await detailsRes.json()) as TVShowDetails;
 
   const externalIdRes = await fetch(
     `https://api.themoviedb.org/3/tv/${showId}/external_ids`,
@@ -44,9 +62,20 @@ export const ShowInfo = async ({ showId }: { showId: string }) => {
   );
 
   return (
-    <div className="p-12">
-      <h1 className="text-lg font-bold">{show.Title}</h1>
-      <div className="flex gap-4">
+    <div className="p-12 flex gap-12 w-full">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-lg font-bold">{show.Title}</h1>
+        {!!showDetails.poster_path && (
+          <div>
+            <img
+              className="w-48 rounded-xl"
+              src={`https://image.tmdb.org/t/p/w500/${showDetails.poster_path}`}
+              alt="poster"
+            />
+          </div>
+        )}
+      </div>
+      <div className="flex gap-4 w-full overflow-x-auto">
         {seasons.map((season) => (
           <Suspense key={season} fallback={<p>loading...</p>}>
             <Season seasonNumber={season} imdbId={imdbId} />
