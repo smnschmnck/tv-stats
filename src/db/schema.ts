72cb18cd412103f6d50ade5a917d91aae75a3eb4
Simon Schmeinck <simon.schmeinck@gmx.de>
2025-09-13 17:47:04 +0200

    add indexes

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,16 +1,42 @@
-import { sqliteTable, text, integer, real } from "drizzle-orm/sqlite-core";
+import {
+  sqliteTable,
+  text,
+  integer,
+  real,
+  index,
+} from "drizzle-orm/sqlite-core";
 
-// tconst (string), averageRating (float), numVotes (int)
-export const ratings = sqliteTable("ratings", {
-  tconst: text("tconst").primaryKey(),
-  averageRating: real("average_rating").notNull(),
-  numVotes: integer("num_votes").notNull(),
-});
+export const ratings = sqliteTable(
+  "ratings",
+  {
+    tconst: text("tconst").primaryKey(),
+    averageRating: real("average_rating").notNull(),
+    numVotes: integer("num_votes").notNull(),
+  },
+  (table) => [
+    index("avg_rating_idx").on(table.averageRating),
+    index("num_votes_idx").on(table.numVotes),
+  ]
+);
 
-// tconst (episode), parentTconst (show/series), seasonNumber, episodeNumber
-export const episodes = sqliteTable("episodes", {
-  tconst: text("tconst").primaryKey(),
-  parentTconst: text("parent_tconst").notNull(),
-  seasonNumber: integer("season_number"),
-  episodeNumber: integer("episode_number"),
-});
+export const episodes = sqliteTable(
+  "episodes",
+  {
+    tconst: text("tconst").primaryKey(),
+    parentTconst: text("parent_tconst")
+      .references(() => ratings.tconst, {
+        onDelete: "cascade",
+        onUpdate: "cascade",
+      })
+      .notNull(),
+    seasonNumber: integer("season_number"),
+    episodeNumber: integer("episode_number"),
+  },
+  (table) => [
+    index("parent_season_episode_idx").on(
+      table.parentTconst,
+      table.seasonNumber,
+      table.episodeNumber
+    ),
+  ]
+);
