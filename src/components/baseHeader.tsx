f2d686360bbd9d684831ea6c993f61b2fdb6d97c
Simon Schmeinck <simon.schmeinck@gmx.de>
2025-09-16 19:36:12 +0200

    add gap to header

--- src/components/baseHeader.tsx
+++ src/components/baseHeader.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 export const BaseHeader = ({ children }: { children?: ReactNode }) => {
   return (
-    <header className="fixed z-10 flex min-h-24 w-full items-center justify-between bg-white px-8 md:px-12">
+    <header className="fixed z-10 flex min-h-24 w-full items-center justify-between gap-4 bg-white px-8 md:px-12">
       <Link className="flex items-center gap-2 font-bold select-none" href="/">
         <Clapperboard />
         <span className="hidden md:block">TV Stats</span>
