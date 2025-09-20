34c5b5222c7a5f17cd8fcccb3fb4d31ab575f961
Simon Schmeinck <simon.schmeinck@gmx.de>
2025-09-20 17:37:03 +0200

    Add translation keys for index page

--- src/app/[locale]/(landing)/page.tsx
+++ src/app/[locale]/(landing)/page.tsx
@@ -2,19 +2,18 @@ import { SearchBar } from "@/components/searchBar";
 import { FullScreenSpinner } from "@/components/ui/spinner";
 import { Suspense } from "react";
 import { PopularShows } from "./components/popularShows";
+import { getTranslations } from "next-intl/server";
 
 export const experimental_ppr = true;
 
-const Hero = () => {
+const Hero = async () => {
+  const t = await getTranslations("IndexPage.hero");
+
   return (
     <div className="flex flex-col gap-8 px-8">
       <div className="flex flex-col gap-2 text-center">
-        <h1 className="text-4xl font-bold">
-          Spend Every Night on a Great Show
-        </h1>
-        <p className="font-medium text-zinc-500">
-          Check IMDb ratings at a glance and find the series worth your time
-        </p>
+        <h1 className="text-4xl font-bold">{t("title")}</h1>
+        <p className="font-medium text-zinc-500">{t("subtitle")}</p>
       </div>
       <div className="w-full sm:px-8">
         <SearchBar />
@@ -23,14 +22,16 @@ const Hero = () => {
   );
 };
 
-const Page = () => {
+const Page = async () => {
+  const t = await getTranslations("IndexPage");
+
   return (
     <main className="flex h-full w-full flex-col items-center gap-16 pt-4">
       <Hero />
       <div>
         <div className="min-h-12 w-full rounded-t-[100%] bg-zinc-50" />
         <div className="flex h-full w-full flex-col items-center gap-8 bg-zinc-50 pb-10">
-          <h2 className="text-xl font-bold">Popular TV Shows</h2>
+          <h2 className="text-xl font-bold">{t("popularShows.title")}</h2>
           <Suspense fallback={<FullScreenSpinner />}>
             <PopularShows />
           </Suspense>
