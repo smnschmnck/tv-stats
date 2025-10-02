9739899848cbd4962d48e0399e3c0a05d1cd54c8
Simon Schmeinck <simon.schmeinck@gmx.de>
2025-10-03 00:12:09 +0200

    refactor i18n logic

---
+++ src/i18n/middleware.ts
@@ -0,0 +1,25 @@
+import type { APIContext } from "astro";
+import i18next from "i18next";
+import Backend from "i18next-fs-backend";
+import { LanguageDetector } from "i18next-http-middleware";
+import { supportedLanguages } from "./constants";
+
+await i18next
+  .use(Backend)
+  .use(LanguageDetector)
+  .init({
+    fallbackLng: "en",
+    backend: {
+      loadPath: "./locales/{{lng}}/translation.json",
+    },
+    preload: supportedLanguages,
+    interpolation: { escapeValue: false },
+  });
+
+export const addTranslations = (context: APIContext) => {
+  const lng = context.currentLocale ?? "en";
+
+  const t = i18next.getFixedT(lng);
+
+  context.locals.t = t;
+};
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,8 @@
 import type { MiddlewareHandler } from "astro";
-import i18next from "i18next";
-import Backend from "i18next-fs-backend";
-import { LanguageDetector } from "i18next-http-middleware";
-import { supportedLanguages } from "./constants/i18n";
-
-await i18next
-  .use(Backend)
-  .use(LanguageDetector)
-  .init({
-    fallbackLng: "en",
-    backend: {
-      loadPath: "./locales/{{lng}}/translation.json",
-    },
-    preload: supportedLanguages,
-    interpolation: { escapeValue: false },
-  });
+import { addTranslations } from "./i18n/middleware";
 
 export const onRequest: MiddlewareHandler = async (context, next) => {
-  const lng = context.currentLocale ?? "en";
-
-  const t = i18next.getFixedT(lng);
-
-  context.locals.t = t;
+  addTranslations(context);
 
   return next();
 };
