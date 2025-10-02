7e79a0b091449669243122c8705e6f9667b08626
Simon Schmeinck <simon.schmeinck@gmx.de>
2025-10-03 00:25:23 +0200

    fix i18n translation loading

--- src/i18n/middleware.ts
+++ src/i18n/middleware.ts
@@ -1,20 +1,18 @@
 import type { APIContext } from "astro";
 import i18next from "i18next";
-import Backend from "i18next-fs-backend";
 import { LanguageDetector } from "i18next-http-middleware";
-import { supportedLanguages } from "./constants";
 
-await i18next
-  .use(Backend)
-  .use(LanguageDetector)
-  .init({
-    fallbackLng: "en",
-    backend: {
-      loadPath: "./locales/{{lng}}/translation.json",
-    },
-    preload: supportedLanguages,
-    interpolation: { escapeValue: false },
-  });
+import de from "../../locales/de/translation.json";
+import en from "../../locales/en/translation.json";
+
+await i18next.use(LanguageDetector).init({
+  fallbackLng: "en",
+  resources: {
+    en: { translation: en },
+    de: { translation: de },
+  },
+  interpolation: { escapeValue: false },
+});
 
 export const addTranslations = (context: APIContext) => {
   const lng = context.currentLocale ?? "en";
