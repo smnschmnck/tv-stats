2ce763b14ed7775dffb05e4ce397d0b77353c733
Simon Schmeinck <simon.schmeinck@gmx.de>
2025-11-01 17:38:15 +0100

    add vaul drawer

--- src/pages/[locale]/show/_components/EpisodeInfoDrawer.tsx
+++ src/pages/[locale]/show/_components/EpisodeInfoDrawer.tsx
@@ -1,12 +1,28 @@
 import { useEffect, useState } from "react";
 import { selectedEpisode } from "../store";
+import { Drawer } from "vaul";
 
-export const MyBla = () => {
+export const EpisodeInfoDrawer = () => {
   const [ep, setEp] = useState(selectedEpisode.get());
 
   useEffect(() => {
     selectedEpisode.subscribe((v) => setEp(v));
   }, []);
 
-  return <p className="h-8 min-w-64 bg-red-500">{JSON.stringify(ep)}</p>;
+  const handleOpenChange = (open: boolean) => {
+    if (!open) {
+      selectedEpisode.set(null);
+    }
+  };
+
+  return (
+    <Drawer.Root open={!!ep} onOpenChange={handleOpenChange}>
+      <Drawer.Portal>
+        <Drawer.Overlay className="fixed inset-0 z-10 bg-black/40" />
+        <Drawer.Content className="fixed right-0 bottom-0 left-0 z-20 h-fit bg-gray-100 outline-none">
+          <div className="bg-white p-4">{JSON.stringify(ep, null, 2)}</div>
+        </Drawer.Content>
+      </Drawer.Portal>
+    </Drawer.Root>
+  );
 };
