4201a3bbf5d1fe8865165150c125ee2a14b75026
Simon Schmeinck <simon.schmeinck@gmx.de>
2025-11-04 02:35:12 +0100

    fix base rating dimensions

--- src/pages/[locale]/show/_components/BaseRating.tsx
+++ src/pages/[locale]/show/_components/BaseRating.tsx
@@ -30,7 +30,7 @@ export const BaseRating = ({
         }
       }}
       className={twMerge(
-        "flex h-10 w-10 items-center justify-center rounded-md bg-zinc-100 font-medium text-white",
+        "flex h-10 min-h-10 w-10 min-w-10 items-center justify-center rounded-md bg-zinc-100 font-medium text-white",
         className,
       )}
     >
